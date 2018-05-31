42438129f06464e0a3036f2e92aa7ede4108aad6
vapalamar <vyacheslav.palamar@gmail.com>
2018-05-31 21:18:38 +0300

    Updated services view layout

--- src/app/services-view/services-view.module.ts
+++ src/app/services-view/services-view.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ServicesViewComponent } from './services-view.component';
 import { ServicesViewRoutingModule } from './services-view-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @NgModule({
   imports: [
     CommonModule,
     ServicesViewRoutingModule,
-    SharedModule
+    SharedModule,
+    BsDropdownModule
   ],
   declarations: [ServicesViewComponent]
 })
