a22373f4df7ed0ccc033b84cd1851bacb284cd2d
vapalamar <vyacheslav.palamar@gmail.com>
2018-06-02 15:56:06 +0300

    Client review list

--- src/app/client-reviews/client-reviews.component.ts
+++ src/app/client-reviews/client-reviews.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { first, mergeMap, map, filter } from 'rxjs/operators';
+import { combineLatest } from 'rxjs/observable/combineLatest';
+import { values } from 'lodash-es';
 
 @Component({
   selector: 'fl-client-reviews',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-reviews.component.scss']
 })
 export class ClientReviewsComponent implements OnInit {
+  reviews: any[];
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDB: AngularFireDatabase
+  ) { }
 
   ngOnInit() {
+    const companies = this.afAuth.authState.pipe(
+      first(),
+      mergeMap(() => this.afDB.database.ref('/users').orderByChild('role').equalTo('vendor').once('value')),
+      map(s => s && s.val())
+    );
+    const userReviews = this.afAuth.authState.pipe(
+      filter(Boolean),
+      first(),
+      mergeMap((u) => this.afDB.database.ref(`users/${u.uid}/my-reviews`).once('value')),
+      map(s => s && s.val())
+    );
+
+    combineLatest(companies, userReviews)
+      .subscribe(([cs, reviews]) => {
+        this.reviews = values(reviews).map(review => ({...review, companyName: cs[review.vendorId].company.name}));
+      });
   }
 
 }
