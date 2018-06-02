2fdcd2c7ec0ff5eba4dcae4037a1a9315f8ed4a3
vapalamar <vyacheslav.palamar@gmail.com>
2018-06-02 15:20:01 +0300

    Reviews list

--- src/app/vendor-reviews/vendor-reviews.component.ts
+++ src/app/vendor-reviews/vendor-reviews.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { filter, first, mergeMap, map } from 'rxjs/operators';
+import { values } from 'lodash-es';
 
 @Component({
   selector: 'fl-vendor-reviews',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendor-reviews.component.scss']
 })
 export class VendorReviewsComponent implements OnInit {
+  reviews: any[];
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDb: AngularFireDatabase
+  ) { }
 
   ngOnInit() {
+    this.afAuth.authState.pipe(
+      filter(Boolean),
+      first(),
+      mergeMap((u) => this.afDb.database.ref(`users/${u.uid}/reviews`).once('value')),
+      map(s => s && s.val())
+    ).subscribe(reviews => {
+      this.reviews = values(reviews);
+    });
   }
 
 }
--- src/app/vendor-reviews/vendor-reviews.module.ts
+++ src/app/vendor-reviews/vendor-reviews.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VendorReviewsComponent } from './vendor-reviews.component';
 import { VendorReviewsRoutingModule } from './vendor-reviews-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    VendorReviewsRoutingModule
+    VendorReviewsRoutingModule,
+    SharedModule
   ],
   declarations: [VendorReviewsComponent]
 })
