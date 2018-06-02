b9626682a956a7ee4faeb551ecb5928a509d28c2
vapalamar <vyacheslav.palamar@gmail.com>
2018-06-02 15:10:45 +0300

    List of orders

--- src/app/vendors-orders/vendors-orders.component.ts
+++ src/app/vendors-orders/vendors-orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { filter, first, mergeMap, map } from 'rxjs/operators';
+import { values } from 'lodash-es';
 
 @Component({
   selector: 'fl-vendors-orders',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vendors-orders.component.scss']
 })
 export class VendorsOrdersComponent implements OnInit {
+  orders: any[];
 
-  constructor() { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afDB: AngularFireDatabase
+  ) { }
 
   ngOnInit() {
+    this.afAuth.authState.pipe(
+      filter(Boolean),
+      first(),
+      mergeMap(u => this.afDB.database.ref(`users/${u.uid}`).once('value')),
+      map(s => s && s.val())
+    ).subscribe(admin => {
+      this.orders = values(admin.orders).map(order => ({
+        ...order,
+        service: admin.services[order.serviceId],
+        masterInfo: admin.employees[order.master]
+      }));
+    });
   }
-
 }
--- src/app/vendors-orders/vendors-orders.module.ts
+++ src/app/vendors-orders/vendors-orders.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VendorsOrdersComponent } from './vendors-orders.component';
 import { VendorsOrdersRoutingModule } from './vendors-orders-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    VendorsOrdersRoutingModule
+    VendorsOrdersRoutingModule,
+    SharedModule
   ],
   declarations: [VendorsOrdersComponent]
 })
